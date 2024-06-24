a09f1c8a9038cf65c466e582aa1081f8c7f91dfd
EmblaSvahnberg <44924572+EmblaSvahnberg@users.noreply.github.com>
2024-06-24 18:10:17 +0200

    Update GameData.cs

--- SceneChange/DataPersistence/GameData.cs
+++ SceneChange/DataPersistence/GameData.cs
@@ -14,21 +14,10 @@ public class GameData
 
     public Vector3 playerPosition;
     
-    public List<QuestSaveData> savedQuests;
-
     //the values defined in this constructur will be the default values the game starts with when there´s no data to load
     public GameData()
     {
         currentScene = null;
         playerPosition = new Vector3(-60, 7, -5);
-        savedQuests = new List<QuestSaveData>();
     }
-}
-
-[System.Serializable]
-public class QuestSaveData
-{
-    public string questId;
-    public QuestState currentState;
-    public string currentProgress;
 }
